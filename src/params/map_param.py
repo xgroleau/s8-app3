da55bc5b873293c9e723565c6db16e97e5e6b65c
xgroleau🐢 <xavgroleau@gmail.com>
2021-10-27 14:54:49 -0400

    added args kwags

--- src/params/map_param.py
+++ src/params/map_param.py
@@ -1,11 +1,11 @@
 from PIL import Image
 from src.images import random_image_selector
 from src.images.image_collection import ImageCollection
-from typing import Callable, Union
+from typing import Callable, Union, Optional
 import numpy as np
 
 
-def map_param(num: int, im_coll: ImageCollection, fun: Callable[[np.ndarray], Union[int, float, np.ndarray]], dim=None):
+def map_param(num: int, im_coll: ImageCollection, fun: Callable[[np.ndarray, ...], Union[int, float, np.ndarray]], dim=None, *args, **kargs):
     indexes = random_image_selector(num, im_coll)
     dim_tuple = (len(indexes), )
     if dim:
@@ -13,5 +13,5 @@ def map_param(num: int, im_coll: ImageCollection, fun: Callable[[np.ndarray], Un
     val = np.zeros(dim_tuple)
     for i in range(len(indexes)):
         imageRGB = np.array(Image.open(im_coll.image_folder + '\\' + im_coll.image_list[indexes[i]]))
-        val[i] = fun(imageRGB)
+        val[i] = fun(imageRGB, *args, **kargs)
     return val
