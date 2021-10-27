246f3fa19fc00013a57397a48576782a998eaab5
Michael <micha.samson@live.ca>
2021-10-26 20:29:55 -0400

    Test avec les diff et du smoothing

--- load_images.py
+++ load_images.py
@@ -12,7 +12,13 @@ from PIL import Image
 from skimage import color as skic
 
 from color_transformation import rgb_to_cmyk
-from plot_utils import plot_1d
+from plot_utils import plot_1d, plot_3d
+
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
 
 
 class ImageCollection:
@@ -41,17 +47,17 @@ def histogrammes(indexes, im_coll):
     '''
     if type(indexes) == int:
         indexes = [indexes]
-        
+
     fig = plt.figure()
     ax = fig.subplots(len(indexes),4)
 
     for num_images in range(len(indexes)):
-    
+
         imageRGB = np.array(Image.open(im_coll.image_folder + '\\' + im_coll.image_list[indexes[num_images]]))
         imageLab = skic.rgb2lab(imageRGB)
         imageHSV = skic.rgb2hsv(imageRGB)
         imageCMYK = rgb_to_cmyk(imageRGB)
-      
+
         # Number of bins per color channel
         n_bins = 256
 
@@ -66,13 +72,13 @@ def histogrammes(indexes, im_coll):
         imageLabhist[:, :, 2] = np.round((imageLab[:, :, 2] - min_ab) * (n_bins - 1) / (max_ab - min_ab))  # ab has all values between -110 and 110
 
         imageHSVhist = np.round(imageHSV*(n_bins-1)) #HSV has all values between 0 and 100
-    
+
         # A list per color channel
         pixel_valuesRGB = np.zeros((3,n_bins))
         pixel_valuesLab = np.zeros((3,n_bins))
         pixel_valuesHSV = np.zeros((3,n_bins))
         pixel_valuesCMYK = np.zeros((4,n_bins))
-    
+
         for i in range(n_bins):
             for j in range(4):
                 if j < 3:
@@ -93,16 +99,17 @@ def histogrammes(indexes, im_coll):
         # ajouter le titre de la photo observée dans le titre de l'histograme
         image_name = im_coll.image_list[indexes[num_images]]
         ax[num_images,0].set_title(f'histogramme RGB de {image_name}')
-        
+
         ax[num_images,1].scatter(range(start,end), pixel_valuesHSV[0,start:end], c='orange')
         ax[num_images,1].scatter(range(start,end), pixel_valuesHSV[1,start:end], c='cyan')
         ax[num_images,1].scatter(range(start,end), pixel_valuesHSV[2,start:end], c='magenta')
         ax[num_images,1].set(xlabel='pixels', ylabel='compte par valeur d\'intensité')
         ax[num_images,1].set_title(f'histogramme HSV de {image_name}')
 
-        ax[num_images, 2].scatter(range(start, end), pixel_valuesLab[0, start:end], c='grey')
-        ax[num_images, 2].scatter(range(start, end), pixel_valuesLab[1, start:end], c='orange')
-        ax[num_images, 2].scatter(range(start, end), pixel_valuesLab[2, start:end], c='violet')
+        x_values = range(start, end-1)
+        ax[num_images, 2].plot(x_values, np.diff(smooth(pixel_valuesRGB[0, start:end], 5)), c='red')
+        ax[num_images, 2].plot(x_values, np.diff(smooth(pixel_valuesRGB[1, start:end], 5)), c='green')
+        ax[num_images, 2].plot(x_values, np.diff(smooth(pixel_valuesRGB[2, start:end], 5)), c='blue')
         ax[num_images, 2].set(xlabel='pixels', ylabel='compte par valeur d\'intensité')
         ax[num_images, 2].set_title(f'histogramme LAB de {image_name}')
 
@@ -122,6 +129,9 @@ def random_image_selector(number, im_coll):
     image_list: liste de strings de 980 items
     number: int
     '''
+    if number == -1:
+        return np.arange(len(im_coll.image_list))
+
     indexes_list = np.random.randint(low=0, high=np.size(im_coll.image_list, 0), size=number)
 
     # Protection contre les doublons
@@ -150,12 +160,21 @@ def images_display(indexes, im_coll):
 
     for i in range(len(indexes)):
         im = Image.open(im_coll.image_folder + '\\' + im_coll.image_list[indexes[i]])
-        ax2[i].imshow(im) 
+        ax2[i].imshow(im)
 
 
 def extract_param(num: int, im_coll: ImageCollection, fun: Callable[[np.ndarray], Union[float]]):
     indexes = random_image_selector(num, im_coll)
     val = np.zeros(len(indexes))
+    for i in range(len(indexes)):
+        imageRGB = np.array(Image.open(im_coll.image_folder + '\\' + im_coll.image_list[indexes[i]]))
+        val[i] = fun(imageRGB)
+    return val
+
+
+def extract_params(num: int, im_coll: ImageCollection, fun: Callable[[np.ndarray], Union[np.ndarray]]):
+    indexes = random_image_selector(num, im_coll)
+    val = np.zeros((len(indexes), 3))
     for i in range(len(indexes)):
         imageRGB = np.array(Image.open(im_coll.image_folder + '\\' + im_coll.image_list[indexes[i]]))
         val[i] = fun(imageRGB)
@@ -171,11 +190,43 @@ def main():
     im_list_street = random_image_selector(6, street)
     images_display(im_list_street, street)
     histogrammes(im_list_street, street)
-    
+
     coast = ImageCollection("coast")
     im_list_coast = random_image_selector(6, coast)
     images_display(im_list_coast, coast)
     histogrammes(im_list_coast, coast)
+
+    def extract_mean(rgb):
+        image = rgb_to_cmyk(rgb)
+        means = [np.mean(image[:, :, 0]), np.mean(image[:, :, 1]), np.mean(image[:, :, 2])]
+        return means
+
+    def extract_std(rgb):
+        image = skic.rgb2lab(rgb)
+        stds = [np.std(image[:, :, 0]), np.std(image[:, :, 1]), np.std(image[:, :, 2])]
+        return stds
+
+    def extract_var(rgb):
+        image = rgb
+        variances = [np.var(image[:, :, 0]), np.var(image[:, :, 1]), np.var(image[:, :, 2])]
+        return variances
+
+    def extract_diff_mean(rgb):
+        image = rgb
+        n_bins = 256
+        pixel_values = np.zeros((3, n_bins))
+        for j in range(4):
+            for i in range(n_bins):
+                if j < 3:
+                    pixel_values[j, i] = np.count_nonzero(image[:, :, j] == i)
+
+        diff_mean = [np.max(np.diff(smooth(pixel_values[0, :], 5))), np.max(np.diff(smooth(pixel_values[1, :], 5))), np.max(np.diff(smooth(pixel_values[2, :], 5)))]
+        return diff_mean
+
+    def extract_cov(rgb):
+        image = skic.rgb2lab(rgb)
+        stds = [np.cov(image[:, :, 0], image[:, :, 1])[0][1], np.cov(image[:, :, 1], image[:, :, 2])[0][1], np.cov(image[:, :, 2], image[:, :, 0])[0][1]]
+        return stds
 
     def extract_mean_ba(rgb):
         imageLab = skic.rgb2lab(rgb)
@@ -187,19 +238,26 @@ def main():
         imagehsv = skic.rgb2hsv(rgb)
         return np.mean(imagehsv[:, :, 1])
 
+    forest_mean = extract_params(200, forest, extract_diff_mean)
+    street_mean = extract_params(200, street, extract_diff_mean)
+    coast_mean = extract_params(200, coast, extract_diff_mean)
+
+    plot_3d(coast_mean, forest_mean, street_mean)
+
+
     # Extract the mean of the blue color and plot the histogram of them
-    forest_mean = extract_param(200, forest, extract_mean_ba)
-    street_mean = extract_param(200, street, extract_mean_ba)
-    coast_mean = extract_param(200, coast, extract_mean_ba)
-
-    plot_1d(coast_mean, forest_mean, street_mean)
-
-    # Extract the mean of the blue color and plot the histogram of them
-    forest_mean = extract_param(200, forest, extract_mean_saturation)
-    street_mean = extract_param(200, street, extract_mean_saturation)
-    coast_mean = extract_param(200, coast, extract_mean_saturation)
-
-    plot_1d(coast_mean, forest_mean, street_mean)
+    # forest_mean = extract_param(200, forest, extract_mean_ba)
+    # street_mean = extract_param(200, street, extract_mean_ba)
+    # coast_mean = extract_param(200, coast, extract_mean_ba)
+    #
+    # plot_1d(coast_mean, forest_mean, street_mean)
+    #
+    # # Extract the mean of the blue color and plot the histogram of them
+    # forest_mean = extract_param(200, forest, extract_mean_saturation)
+    # street_mean = extract_param(200, street, extract_mean_saturation)
+    # coast_mean = extract_param(200, coast, extract_mean_saturation)
+    #
+    # plot_1d(coast_mean, forest_mean, street_mean)
 
     plt.show()
 
--- plot_utils.py
+++ plot_utils.py
@@ -4,9 +4,18 @@ import numpy as np
 
 def plot_1d(coasts, forests, streets, bins=100):
     plt.figure()
-    plt.hist([coasts, forests, streets], bins, alpha=0.5, label=["coasts", "forests", "streets"], color=["blue", "green", "grey"])
-
+    plt.hist([coasts, forests, streets], bins, alpha=0.5, label=["coasts", "forests", "streets"], color=["blue", "green", "red"])
 
 def plot_2d(coasts, forests, streets):
     ax = plt.axes(projection='3d')
     ax.scatter(coasts, forests, streets)
+
+def plot_3d(coasts, forests, streets):
+    plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.scatter(coasts[:,0], coasts[:,1], coasts[:,2], alpha=0.5, label="coasts", color="blue")
+    ax.scatter(forests[:,0], forests[:,1], forests[:,2], alpha=0.5, label="forests", color="green")
+    ax.scatter(streets[:,0], streets[:,1], streets[:,2], alpha=0.5, label="streets", color="red")
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
