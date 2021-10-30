71ba5673b93444afee2e36637884e8393011582a
Charles Quesnel <quesnel.charles@gmail.com>
2021-10-30 10:16:45 -0400

    Allow multiple sets of kwargs

--- src/main.py
+++ src/main.py
@@ -44,37 +44,37 @@ def main():
 
     categorized_collection = {"coast": coast, "forest": forest, "street": street}
 
-    param_1d(categorized_collection, extract_peak_hsv, subset_start=5, dimension=0)
-    param_1d(categorized_collection, extract_peak_std_hsv, dimension=0)
-    param_1d(categorized_collection, extract_peak_cmyk, subset_start=5, dimension=2)
-    param_1d(categorized_collection, extract_peak_lab, subset_start=100, subset_end=150, dimension=1)
-    param_1d(categorized_collection, extract_peak_lab, subset_start=100, subset_end=150, dimension=2)
-    param_1d(categorized_collection, extract_peak_height_cmyk, subset_start=0, subset_end=50, dimension=0)
-    param_1d(categorized_collection, extract_peak_height_cmyk, subset_start=0, subset_end=50, dimension=1)
-    param_1d(categorized_collection, extract_mean_hsv, dimension=1)
+    param_1d(categorized_collection, (extract_peak_hsv, {'subset_start':5, 'dimension':0}))
+    # param_1d(categorized_collection, extract_peak_std_hsv, dimension=0)
+    # param_1d(categorized_collection, extract_peak_cmyk, subset_start=5, dimension=2)
+    # param_1d(categorized_collection, extract_peak_lab, subset_start=100, subset_end=150, dimension=1)
+    # param_1d(categorized_collection, extract_peak_lab, subset_start=100, subset_end=150, dimension=2)
+    # param_1d(categorized_collection, extract_peak_height_cmyk, subset_start=0, subset_end=50, dimension=0)
+    # param_1d(categorized_collection, extract_peak_height_cmyk, subset_start=0, subset_end=50, dimension=1)
+    # param_1d(categorized_collection, extract_mean_hsv, dimension=1)
     # param_3d(categorized_collection, extract_mean_count_pixel_in_slice, subset_start=0, subset_end=75, num_images=-1, title="RGB Slice sum 0 - 75")
     # param_3d(categorized_collection, extract_mean_count_pixel_in_slice, subset_start=75, subset_end=250, num_images=-1, title="RGB Slice sum 75 - 250")
     # get_images(categorized_collection, extract_param_pixels, num_images=-1, title="HSV params pixel")
 
-    knn = knn_classifier(params, n_neighbors=5)
-    kmean = kmean_clustering(params, n_cluster=5)
-    bayes = BayesianClassifier(params)
-
-    saturation_coast = map_param(30, categorized_collection['coast'], extract_mean_saturation)
-    saturation_forest = map_param(30, categorized_collection['forest'], extract_mean_saturation)
-    saturation_street = map_param(30, categorized_collection['street'], extract_mean_saturation)
-
-    bayes_fit_coasts = bayes.fit_multiple(saturation_coast, likelihood='gaussian')
-    bayes_fit_forests = bayes.fit_multiple(saturation_forest, likelihood='gaussian')
-    bayes_fit_streets = bayes.fit_multiple(saturation_street, likelihood='gaussian')
-
-    ratio_coasts = np.sum((bayes_fit_coasts == 0)) / len(bayes_fit_coasts)
-    ratio_forest = np.sum((bayes_fit_forests == 1)) / len(bayes_fit_forests)
-    ratio_streets = np.sum((bayes_fit_streets == 2)) / len(bayes_fit_streets)
-
-    print(f"{ratio_coasts} Arr: {bayes_fit_coasts}")
-    print(f"{ratio_forest} Arr: {bayes_fit_forests}")
-    print(f"{ratio_streets} Arr: {bayes_fit_streets}")
+    # knn = knn_classifier(params, n_neighbors=5)
+    # kmean = kmean_clustering(params, n_cluster=5)
+    # bayes = BayesianClassifier(params)
+    #
+    # saturation_coast = map_param(30, categorized_collection['coast'], extract_mean_saturation)
+    # saturation_forest = map_param(30, categorized_collection['forest'], extract_mean_saturation)
+    # saturation_street = map_param(30, categorized_collection['street'], extract_mean_saturation)
+    #
+    # bayes_fit_coasts = bayes.fit_multiple(saturation_coast, likelihood='gaussian')
+    # bayes_fit_forests = bayes.fit_multiple(saturation_forest, likelihood='gaussian')
+    # bayes_fit_streets = bayes.fit_multiple(saturation_street, likelihood='gaussian')
+    #
+    # ratio_coasts = np.sum((bayes_fit_coasts == 0)) / len(bayes_fit_coasts)
+    # ratio_forest = np.sum((bayes_fit_forests == 1)) / len(bayes_fit_forests)
+    # ratio_streets = np.sum((bayes_fit_streets == 2)) / len(bayes_fit_streets)
+    #
+    # print(f"{ratio_coasts} Arr: {bayes_fit_coasts}")
+    # print(f"{ratio_forest} Arr: {bayes_fit_forests}")
+    # print(f"{ratio_streets} Arr: {bayes_fit_streets}")
 
     plt.show()
 
