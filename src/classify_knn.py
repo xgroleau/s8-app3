f9f4028179e74c9d6d4ed673b7849e7ee4e9d692
xgroleau🐢 <xavgroleau@gmail.com>
2021-10-31 16:22:48 -0400

    wip

--- src/classify_knn.py
+++ src/classify_knn.py
@@ -15,6 +15,9 @@ import pickle as pkl
 
 from src.visualization import plot_sub_params
 
+
+RELOAD_PARAMS = False
+
 sys.path.append('../')
 CDIR = os.path.dirname(os.path.realpath(__file__))
 images_path = os.path.join(CDIR, '../baseDeDonneesImages')
@@ -25,18 +28,23 @@ street = ImageCollection(base_path=images_path, filter_name="street")
 
 categorized_collection = {"coast": coast, "forest": forest, "street": street}
 
-# params = param_nd(categorized_collection, [(extract_peak_hsv, {'subset_start': 5, 'dimension': 0}),
-#                                            (extract_peak_std_hsv, {'dimension': 0}),
-#                                            (extract_peak_cmyk, {'subset_start': 5, 'dimension': 2}),
-#                                            (extract_peak_lab, {'subset_start': 100, 'subset_end': 150, 'dimension': 1}),
-#                                            (extract_peak_lab, {'subset_start': 100, 'subset_end': 150, 'dimension': 2}),
-#                                            (extract_peak_height_cmyk, {'subset_start': 0, 'subset_end': 50, 'dimension': 0}),
-#                                            (extract_peak_height_cmyk, {'subset_start': 0, 'subset_end': 50, 'dimension': 1}),
-#                                            (extract_mean_hsv, {'dimension': 1}), ], num_images=-1)
+if RELOAD_PARAMS:
+    params = param_nd(categorized_collection, [(extract_peak_hsv, {'subset_start': 5, 'dimension': 0}),
+                                               (extract_peak_std_hsv, {'dimension': 0}),
+                                               (extract_peak_cmyk, {'subset_start': 5, 'dimension': 2}),
+                                               (extract_peak_lab, {'subset_start': 100, 'subset_end': 150, 'dimension': 1}),
+                                               (extract_peak_lab, {'subset_start': 100, 'subset_end': 150, 'dimension': 2}),
+                                               (extract_peak_height_cmyk, {'subset_start': 0, 'subset_end': 50, 'dimension': 0}),
+                                               (extract_peak_height_cmyk, {'subset_start': 0, 'subset_end': 50, 'dimension': 1}),
+                                               (extract_mean_hsv, {'dimension': 1}), ], num_images=-1)
 
-# a_file = open("params.pkl", "wb")
-# pkl.dump(params, a_file)
-# a_file.close()
+    f = open("params.pkl", "wb")
+    pkl.dump(params, f)
+    f.close()
+else:
+    f = open("params.pkl", "rb")
+    params = pkl.load(f)
+    f.close()
 
 a_file = open("params.pkl", "rb")
 params = pkl.load(a_file)
@@ -59,10 +67,11 @@ class_representant = kmean_clustering(params, n_cluster=10)
 kNN = KNNClassifier(n_neighbors=1)
 kNN.fit(class_representant)
 
-plot_sub_params(params, (0, 5, 6), param_labels, cluster_center=class_representant)
-#plot_sub_params(params, (0, 5, 6), param_labels, cluster_center=class_representant)
-#plot_sub_params(params, 2, param_labels, cluster_center=class_representant)
+plot_sub_params(params, (0, 1, 2), param_labels, cluster_center=class_representant, title="012")
+plot_sub_params(params, (3, 4, 5), param_labels, cluster_center=class_representant, title="345")
+plot_sub_params(params, (6, 7), param_labels, cluster_center=class_representant, title="67")
 
-create_confusion_matrix(params, kNN.predict, display=True)
+
+create_confusion_matrix(params, kNN.predict, display=True, normalize="true")
 
 plt.show()
