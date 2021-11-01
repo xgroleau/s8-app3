5086a34a2ffa81597ee66b3b17870c4cba1130de
Michael <micha.samson@live.ca>
2021-11-01 10:05:31 -0400

    WIP

--- src/classify_bayes.py
+++ src/classify_bayes.py
@@ -31,22 +31,14 @@ param_labels = ["extractor_unique", "extractor_mean r", "extractor_mean g", "ext
 
 RELOAD_PARAMS = False
 if RELOAD_PARAMS:
-    params = param_nd(categorized_collection, [(extractor_unique, {'dimension': 0, 'base_function': rgb_to_cmyk}),
-                                               (extractor_unique, {'dimension': 1, 'base_function': rgb_to_cmyk}),
-                                               (extractor_unique, {'dimension': 2, 'base_function': rgb_to_cmyk}),
-                                               (extractor_unique, {'dimension': 3, 'base_function': rgb_to_cmyk}),
-                                               (extractor_mean, {'dimension': 0, 'base_function': rgb_to_cmyk}),
-                                               (extractor_mean, {'dimension': 1, 'base_function': rgb_to_cmyk}),
+    params = param_nd(categorized_collection, [(extractor_mean, {'dimension': 1, 'base_function': skic.rgb2xyz}),
                                                (extractor_mean, {'dimension': 2, 'base_function': rgb_to_cmyk}),
-                                               (extractor_mean, {'dimension': 3, 'base_function': rgb_to_cmyk}),
-                                               (extractor_median, {'dimension': 0, 'base_function': rgb_to_cmyk}),
-                                               (extractor_median, {'dimension': 1, 'base_function': rgb_to_cmyk}),
-                                               (extractor_median, {'dimension': 2, 'base_function': rgb_to_cmyk}),
-                                               (extractor_median, {'dimension': 3, 'base_function': rgb_to_cmyk}),
-                                               (extractor_std, {'dimension': 0, 'base_function': rgb_to_cmyk}),
-                                               (extractor_std, {'dimension': 1, 'base_function': rgb_to_cmyk}),
-                                               (extractor_std, {'dimension': 2, 'base_function': rgb_to_cmyk}),
-                                               (extractor_std, {'dimension': 3, 'base_function': rgb_to_cmyk})], num_images=-1)
+                                               (extractor_mean, {'dimension': 0, }),
+                                               (extractor_mean, {'dimension': 1, }),
+                                               (extractor_mean, {'dimension': 2, }),
+                                               (extractor_std, {'dimension': 2, 'base_function': skic.rgb2hsv}),
+                                               (extractor_std, {'dimension': 3, 'base_function': rgb_to_cmyk}),
+                                               ], num_images=-1)
 
     f = open("params.pkl", "wb")
     pkl.dump(params, f)
@@ -57,8 +49,12 @@ else:
     f.close()
 
 
-# Keep extractor_unique, rgb mean, rgb median peut remove median et unique xyz
-# params = param_remove_unused(params, [0,1,2,6,7,8])
+# Keep extractor_unique, rgb mean, rgb median peut remove median et unique xyz proposition: hsv 11, xyz 4, cmyk 6, cmyk 15
+# CMYK Utile pour differencier coast 4, (6 -7), 9, 12, 13, (15 -9)
+# CMYK Utile pour differencier street 5, 6, 8
+# xyz forest 3, (4), 5
+# hsv 2 (11)
+params = param_remove_unused(params, [])
 
 # params = param_nd(categorized_collection, [(extract_peak_hsv, {'subset_start': 0, 'dimension': 0}),
 #                                            (extract_peak_lab, {'dimension': 2}), ], num_images=-1)
@@ -69,7 +65,7 @@ bayes = BayesianClassifier(params, bins=1)
 #create_confusion_matrix(params, bayes.fit_multiple, display=True, agregate=False, likelihood='gaussian')
 #create_confusion_matrix(params, bayes.fit_multiple, display=True, agregate=True, likelihood='gaussian')
 
-view = (0, 1, 2)
+view = (3,4,6)
 
 # for k, v in params.items():
 #     params[k]['params'][:, 0] = (params[k]['params'][:, 0] + 50) % 255
@@ -80,9 +76,9 @@ view = (0, 1, 2)
 plot_sub_params(params, view, param_labels)
 #plot_sub_params(params, 2, param_labels)
 #plot_sub_params(params, 3, param_labels)
-# params = subclass(params, 'coast', subclass_param_threshold, param_idx=0, threshold=0.6)
-# params = subclass(params, 'street', subclass_param_threshold, param_idx=0, threshold=0.6)
-# params = subclass(params, 'forest', subclass_param_threshold, param_idx=0, threshold=0.6)
+# params = subclass(params, 'coast', subclass_param_threshold, param_idx=1, threshold=89)
+# params = subclass(params, 'street', subclass_param_threshold, param_idx=0, threshold=0.3)
+# params = subclass(params, 'forest', subclass_param_threshold, param_idx=0, threshold=0.3)
 # params = subclass(params, 'street_0', subclass_param_threshold, param_idx=4, threshold=4000)
 # params = subclass(params, 'forest_0', subclass_param_threshold, param_idx=2, threshold=6000)
 #params = subclass(params, 'forest_0', subclass_param_threshold, param_idx=2, threshold=50)
